388d7d6515563f6d8be97ba8fabf2343b76762f2
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2023-09-24 17:58:15 -0500

    sort of working

--- AudioProcessor.cs
+++ AudioProcessor.cs
@@ -124,14 +124,39 @@ namespace VstNetAudioPlugin
             }
         }
 
+        double[] notes =
+        {16.35,17.32,18.35,19.45,20.6,21.83,23.12,24.5,25.96,27.5,29.14,30.87
+        ,32.7,34.65,36.71,38.89,41.2,43.65,46.25,49,51.91,55,58.27,61.74
+        ,65.41,69.3,73.42,77.78,82.41,87.31,92.5,98,103.83,110,116.54,123.47
+        ,130.81,138.59,146.83,155.56,164.81,174.61,185,196,207.65,220,233.08,246.94
+        ,261.63,277.18,293.66,311.13,329.63,349.23,369.99,392,415.3,440,466.16,493.88
+        ,523.25,554.37,587.33,622.25,659.25,698.46,739.99,783.99,830.61,880,932.33,987.77
+        ,1046.5,1108.73,1174.66,1244.51,1318.51,1396.91,1479.98,1567.98,1661.22,1760,1864.66,1975.53
+        ,2093,2217.46,2349.32,2489,2637,2793.83,2959.96,3135.96,3322.44,3520,3729.31,3951
+        ,4186,4434.92,4698.63,4978,5274,5587.65,5919.91,6271.93,6644.88,7040,7458.62,7902.13
+        ,8372,8869.84,9397.26,9956,10548,11175.3,11839.82,12543.86,13289.76,14080,14917.24,15804.26
+        ,16744,17739.68,18794.52,19912};
+        float[] noteIntensity = { };
+        byte[] colorBuffer = { };
 
         // Create arrays for complex data and FFT result
-        const int bufferSize = 256;
+        const int bufferSize = 512;
         Complex32[] buffer = new Complex32[bufferSize]; // Using Complex32 type for complex numbers
         int head = 0;
+        double[] magnitudes = new double[bufferSize / 2];
+
         // process a single audio channel
         private void Process(Delay delay, VstAudioBuffer input, VstAudioBuffer output)
         {
+            if(noteIntensity.Length == 0)
+            {
+                noteIntensity = new float[notes.Length];
+            }
+            if(colorBuffer.Length == 0)
+            {
+                colorBuffer = new byte[notes.Length];
+            }
+            
             for (int i = 0; i < input.SampleCount; i++)
             {
                 buffer[head++] = input[i];
@@ -144,25 +169,48 @@ namespace VstNetAudioPlugin
             if (head != 0) return;
 
             // Perform FFT
-            Fourier.Forward(buffer, FourierOptions.Default);
+            Fourier.Forward(buffer, FourierOptions.NoScaling);
 
-            double[] magnitudes = new double[buffer.Length / 2];
-            int maxIndex = 0;
+            
             double maxMag = 0;
             for (int i = 0; i < magnitudes.Length; i++)
             {
                 magnitudes[i] = buffer[i].Magnitude;
                 if (magnitudes[i] > maxMag)
                 {
-                    maxIndex = i;
                     maxMag = magnitudes[i];
                 }
             }
 
-            byte[] colorBuffer = new byte[magnitudes.Length];
-            for (int i = 0; i < colorBuffer.Length; i++)
+
+            float hzPerSample = SampleRate / bufferSize / 10;
+            float currentHz = 0;
+            int currNote = 0;
+            int sampleCount = 0;
+            Array.Clear(noteIntensity, 0, noteIntensity.Length);
+            for (int i = 0; i < magnitudes.Length; i++)
             {
-                colorBuffer[i] = (byte)(magnitudes[i] / 10 * 255 + 0.5);
+                if (currNote >= notes.Length - 1) break;
+                if(currentHz >= notes[currNote])
+                {
+                    noteIntensity[currNote] /= sampleCount;
+                    sampleCount = 0;
+                    currNote += 1;
+                }
+                //if the current sample is less than the specified note frequency
+                if(currentHz < notes[currNote])
+                {
+                    //track the amount of samples so we can average
+                    sampleCount += 1;
+                    //Add to the note
+                    noteIntensity[currNote] += (float)magnitudes[i];
+                }
+                currentHz += hzPerSample;
+            }
+
+            for (int i = 0; i < noteIntensity.Length; i++)
+            {
+                colorBuffer[i] = (byte)(noteIntensity[i] * 255 + 0.5);
             }
 
             Delay.networking.SendByteArray("FFT" + delay.GetIndex(), colorBuffer);
